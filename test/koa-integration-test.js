444f62232c7c2d2c5490cd39b5ebe0a1bdff42d7
Charlie Duong <charlieduong94@gmail.com>
2017-05-02 21:07:32 -0400

    Added tests for koa integration

---
+++ test/koa-integration-test.js
@@ -0,0 +1,94 @@
+const https = require('https')
+const Buffer = require('buffer')
+const fs = require('fs')
+const { expect } = require('chai')
+
+const Router = require('../src/Router')
+
+const Koa = require('koa')
+
+const { METHODS } = require('http')
+
+describe('koa integration', () => {
+  let router
+  let server
+  let port
+
+  const path = '/api/v1/test'
+
+  function request (method) {
+    let requestOptions = {
+      hostname: 'localhost',
+      port,
+      path,
+      method,
+      rejectUnauthorized: false
+    }
+
+    return new Promise((resolve, reject) => {
+      const req = https.request(requestOptions, (res) => {
+        let data = ''
+
+        res.setEncoding('utf8')
+
+        res.on('data', (chunk) => { data += chunk })
+        res.on('end', () => {
+          if (data.length === 0) {
+            resolve(null)
+          } else {
+            resolve(JSON.parse(data))
+          }
+        })
+      })
+
+      req.on('error', reject)
+      req.end()
+    })
+  }
+
+  before('setup routes', () => {
+    router = new Router()
+
+    for (const method of METHODS) {
+      router[method.toLowerCase()](path, (ctx) => {
+        ctx.body = {
+          method
+        }
+      })
+    }
+  })
+
+  before('set http server', (done) => {
+    const app = new Koa()
+    app.use(router.getRequestHandler())
+
+    const options = {
+      key: fs.readFileSync(require.resolve('./util/key.pem')),
+      cert: fs.readFileSync(require.resolve('./util/cert.pem'))
+    }
+
+    server = https.createServer(options, app.callback()).listen(0, function () {
+      port = this.address().port
+      done()
+    })
+  })
+
+  after(() => {
+    server.close()
+  })
+
+  it('should be able to handle http requests', async () => {
+    for (const method of METHODS) {
+      // unable to handle connect requests
+      if (method !== 'CONNECT') {
+        const response = await request(method)
+
+        if (method !== 'HEAD') {
+          expect(response).to.deep.equal({ method })
+        } else {
+          expect(response).to.equal(null)
+        }
+      }
+    }
+  })
+})
